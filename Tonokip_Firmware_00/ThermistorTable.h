ce6efde699c3f02d5b98b4116fc8c692b720f0d7
jmgiacalone <info@emakershop.com>
2011-02-19 00:09:53 +0000

    runs on repstrap

---
+++ Tonokip_Firmware_00/ThermistorTable.h
@@ -0,0 +1,169 @@
+#ifndef THERMISTORTABLE_H_
+#define THERMISTORTABLE_H_
+
+// Thermistor lookup table for RepRap Temperature Sensor Boards (http://make.rrrf.org/ts)
+// See this page:  
+// http://dev.www.reprap.org/bin/view/Main/Thermistor
+// for details of what goes in this table.
+// Made with createTemperatureLookup.py (http://svn.reprap.org/trunk/reprap/firmware/Arduino/utilities/createTemperatureLookup.py)
+// ./createTemperatureLookup.py --r0=100000 --t0=25 --r1=0 --r2=4700 --beta=4066 --max-adc=1023
+// r0: 100000
+// t0: 25
+// r1: 0
+// r2: 4700
+// beta: 4450
+// max adc: 1023
+// UltiMachine.com Thermistor
+#ifndef REPSTRAP
+#define NUMTEMPS 33
+short _thTempTable[NUMTEMPS][2] = {
+
+{704,155},
+{714,150},
+{724,145},
+{734,140},
+{744,135},
+{754,130},
+{764,125},
+{774,120},
+{784,115},
+{794,110},
+{804,105},
+{814,100},
+{824,95},
+{834,90},
+{844,85},
+{854,80},
+{864,75},
+{874,70},
+{884,65},
+{894,60},
+{904,55},
+{914,50},
+{924,45},
+{934,40},
+{944,35},
+{954,30},
+{964,25},
+{974,20},
+{984,15},
+{994,10},
+{1004,5},
+{1014,0},
+{1024,-5}
+};
+
+ 
+/*
+// EPCOS 100K Thermistor (B57560G1104F)
+// Made with createTemperatureLookup.py (http://svn.reprap.org/trunk/reprap/firmware/Arduino/utilities/createTemperatureLookup.py)
+// ./createTemperatureLookup.py --r0=100000 --t0=25 --r1=0 --r2=4700 --beta=4092 --max-adc=1023
+// r0: 100000
+// t0: 25
+// r1: 0
+// r2: 4700
+// beta: 4092
+// max adc: 1023
+short _thNTempTable[NUMTEMPS][2] = {
+   {1, 821},
+   {54, 252},
+   {107, 207},
+   {160, 182},
+   {213, 165},
+   {266, 152},
+   {319, 141},
+   {372, 131},
+   {425, 123},
+   {478, 115},
+   {531, 107},
+   {584, 100},
+   {637, 93},
+   {690, 86},
+   {743, 78},
+   {796, 70},
+   {849, 60},
+   {902, 49},
+   {955, 34},
+   {1008, 3}
+ };
+ */
+// Thermistor lookup table for RepRap Temperature Sensor Boards (http://make.rrrf.org/ts)
+// Made with createTemperatureLookup.py (http://svn.reprap.org/trunk/reprap/firmware/Arduino/utilities/createTemperatureLookup.py)
+// ./createTemperatureLookup.py --r0=100000 --t0=25 --r1=0 --r2=1000 --beta=4092 --max-adc=1023
+// r0: 100000
+// t0: 25
+// r1: 0
+// r2: 1000
+// beta: 4092
+// max adc: 1023
+#define NUMTEMPS 40
+short  _thNTempTable[NUMTEMPS][2] = {
+   {1, 1596},
+   {27, 469},
+   {53, 385},
+   {79, 343},
+   {105, 315},
+   {131, 295},
+   {157, 279},
+   {183, 265},
+   {209, 254},
+   {235, 244},
+   {261, 235},
+   {287, 227},
+   {313, 219},
+   {339, 213},
+   {365, 206},
+   {391, 200},
+   {417, 194},
+   {443, 189},
+   {469, 184},
+   {495, 178},
+   {521, 173},
+   {547, 168},
+   {573, 164},
+   {599, 159},
+   {625, 154},
+   {651, 149},
+   {677, 144},
+   {703, 140},
+   {729, 135},
+   {755, 129},
+   {781, 124},
+   {807, 119},
+   {833, 113},
+   {859, 106},
+   {885, 99},
+   {911, 91},
+   {937, 82},
+   {963, 71},
+   {989, 55},
+   {1015, 22}
+};
+
+#else
+#define NUMTEMPS 20
+short _thTempTable[NUMTEMPS][2] = {
+
+   {1, 628},
+   {54, 222},
+   {107, 184},
+   {160, 163},
+   {213, 149},
+   {266, 138},
+   {319, 128},
+   {372, 120},
+   {425, 112},
+   {478, 106},
+   {531, 99},
+   {584, 93},
+   {637, 86},
+   {690, 80},
+   {743, 73},
+   {796, 66},
+   {849, 57},
+   {902, 47},
+   {955, 33},
+   {1008, 4}
+ };
+#endif
+#endif
---
+++ Tonokip_Firmware_00/configuration.h
@@ -0,0 +1,74 @@
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#define DEBUG false
+
+// NO RS485/EXTRUDER CONTROLLER SUPPORT
+// PLEASE VERIFY PIN ASSIGNMENTS FOR YOUR CONFIGURATION!!!!!!!
+#define MOTHERBOARD 3 // ATMEGA168 0, SANGUINO 1, MOTHERBOARD = 2, MEGA 3, ATMEGA328 4
+
+// THERMOCOUPLE SUPPORT UNTESTED... USE WITH CAUTION!!!!
+const bool USE_THERMISTOR = true; //Set to false if using thermocouple
+
+// Calibration formulas
+// e_extruded_steps_per_mm = e_feedstock_steps_per_mm * (desired_extrusion_diameter^2 / feedstock_diameter^2)
+// new_axis_steps_per_mm = previous_axis_steps_per_mm * (test_distance_instructed/test_distance_traveled)
+// units are in millimeters or whatever length unit you prefer: inches,football-fields,parsecs etc
+
+//Calibration variables
+float x_steps_per_unit = 80;
+float y_steps_per_unit = 80;
+float z_steps_per_unit = 4571.43;
+float e_steps_per_unit = 65.304;
+float max_feedrate = 18000;
+
+//float x_steps_per_unit = 10.047;
+//float y_steps_per_unit = 10.047;
+//float z_steps_per_unit = 833.398;
+//float e_steps_per_unit = 0.706;
+//float max_feedrate = 3000;
+
+//For Inverting Stepper Enable Pins (Active Low) use 0, Non Inverting (Active High) use 1
+const bool X_ENABLE_ON = 0;
+const bool Y_ENABLE_ON = 0;
+const bool Z_ENABLE_ON = 0;
+const bool E_ENABLE_ON = 0;
+
+//Disables axis when it's not being used.
+const bool DISABLE_X = false;
+const bool DISABLE_Y = false;
+const bool DISABLE_Z = true;
+const bool DISABLE_E = false;
+
+const bool INVERT_X_DIR = true;
+const bool INVERT_Y_DIR = false;
+const bool INVERT_Z_DIR = true;
+const bool INVERT_E_DIR = false;
+
+//Endstop Settings
+const bool ENDSTOPS_INVERTING = 0;
+const bool min_software_endstops = false; //If true, axis won't move to coordinates less than zero.
+const bool max_software_endstops = true;  //If true, axis won't move to coordinates greater than the defined lengths below.
+const int X_MAX_LENGTH = 140;
+const int Y_MAX_LENGTH = 140;
+const int Z_MAX_LENGTH = 100;
+
+//Comms settings
+#define BAUDRATE 115200
+#define MAX_CMD_SIZE 256
+
+#define HEAT_INTERVAL 225 // extruder control interval in milliseconds
+#define LZONE 0
+#define UZONE 1
+
+//RESISTOR 12 OHM
+#define PID_MAX 255 // limits current to nozzle
+#define PID_INTEGRAL_DRIVE_MAX 220 //200
+#define PID_PGAIN 2.0 //1.8
+#define PID_IGAIN 0.02//0.02
+#define PID_DGAIN 1.0//1.0
+
+#define TEMP_MULTIPLIER 4 //1
+#define NZONE 5
+
+#endif
